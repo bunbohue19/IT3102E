02d6c3f3c96c88ecfe0e350e724e1b05c4ecc149
bunbohue19 <bunbohue1906@outlook.com>
2022-06-15 10:53:29 +0700

    checked

--- Lab08/AimsProject/src/hust/soict/globalict/aims/media/Book.java
+++ Lab08/AimsProject/src/hust/soict/globalict/aims/media/Book.java
@@ -1,9 +1,6 @@
 package hust.soict.globalict.aims.media;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Book extends Media implements Comparable {
     private ArrayList<String> authors = new ArrayList<String>();
@@ -11,15 +8,20 @@ public class Book extends Media implements Comparable {
 
     private String[] contentTokens;
 
+    private Map<String, Integer> wordFrequency = new TreeMap<String, Integer>();
+
     public Book() {
 
     }
-
     public Book(String title, String category, float cost, ArrayList<String> authors) {
         super(title, category, cost);
         this.authors = authors;
     }
 
+    public Book(String title, String category, float cost, ArrayList<String> authors, String content) {
+        this(title, category, cost, authors);
+        this.content = content;
+    }
     public String authorsInfo() {
         return authors.toString();
     }
@@ -28,6 +30,9 @@ public class Book extends Media implements Comparable {
         return content;
     }
 
+    public void setContent() {
+        this.content = content;
+    }
     public void setContent(String content) {
         this.content = content;
     }
@@ -40,10 +45,33 @@ public class Book extends Media implements Comparable {
         }
         return 0;
     }
-
     public void processContent(){
         List<String> contentTokens = new ArrayList<String>();
-        Map<String, Integer> wordFrequency = new HashMap<>();
-        this.contentTokens = this.getContent().split(" ");
+        int[] frequency = {1};
+        this.contentTokens = content.split(" ");
+        for (int i = 0; i < contentTokens.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(contentTokens.get(i).equalsIgnoreCase(contentTokens.get(j))) {
+                    wordFrequency.put(contentTokens.get(j), frequency[j]++);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String[] tokenList = {null};
+        int i = 0;
+        for (String key : wordFrequency.keySet()) {
+            tokenList[i] = key;
+            i++;
+        }
+        return  "- Title: " + getTitle() +
+                "\n- Category: " + getCategory() +
+                "\n- Cost: " + getCost() +
+                "\n- Authors: " + authorsInfo() +
+                "\n- The content length (the number of tokens): " + this.contentTokens.length +
+                "\n- The token list: " + Arrays.toString(tokenList) +
+                "\n- The word frequency: " + wordFrequency.toString();
     }
 }
--- Lab08/AimsProject/src/hust/soict/globalict/test/book/BookTest.java
+++ Lab08/AimsProject/src/hust/soict/globalict/test/book/BookTest.java
@@ -1,5 +1,18 @@
 package hust.soict.globalict.test.book;
 
-public class BookTest {
+import hust.soict.globalict.aims.media.Book;
+import hust.soict.globalict.aims.media.Media;
+import hust.soict.globalict.aims.media.factory.BookCreation;
+import static hust.soict.globalict.aims.Aims.createMedia;
 
+public class BookTest {
+    public static void main(String[] args) {
+        BookCreation bookCreation = new BookCreation();
+        Media bookMedia = createMedia(bookCreation);
+        Book book = (Book) bookMedia;
+        String content = "And when you get to topping I see that you've been learning And when I take you shopping You spend it like you earned it And when you popped off on your ex, he deserved it I thought you were the one from the jump that confirmed it";
+        book.setContent(content);
+        book.processContent();
+        System.out.println(book.toString());
+    }
 }
